92f56e0549dc8bd3a648cf0f2785d08eb596223a
Simon Jupp <simon.jupp@gmail.com>
2015-04-08 08:23:10 +0100

    more url fixes

--- webulous-mvc/src/main/java/uk/ac/ebi/spot/webulous/controller/HomeController.java
+++ webulous-mvc/src/main/java/uk/ac/ebi/spot/webulous/controller/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("")
 public class HomeController {
-    @RequestMapping("")
+    @RequestMapping({"", "index"})
     public String showHome() {
         return "index";
     }
