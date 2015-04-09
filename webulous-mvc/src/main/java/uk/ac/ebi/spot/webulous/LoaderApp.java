c9b91a0d90de29ecfef8a3f9b4e76a5a291cd47b
Simon Jupp <simon.jupp@gmail.com>
2015-04-09 22:57:45 +0100

    added EFO cell line example

--- webulous-mvc/src/main/java/uk/ac/ebi/spot/webulous/LoaderApp.java
+++ webulous-mvc/src/main/java/uk/ac/ebi/spot/webulous/LoaderApp.java
@@ -35,7 +35,162 @@ public class LoaderApp  implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
+        restrictionRunRepository.deleteAll();
+        dataRunRepository.deleteAll();
+        templateRepository.deleteAll();
 
+        createPizza();
+        createEFOCellLine();
+
+
+
+
+
+    }
+
+    private void createEFOCellLine() {
+        System.out.println("creating a new template for EFO");
+        PopulousTemplateDocument document = new PopulousTemplateDocument();
+        document.setDescription("EFO Cell Line");
+        document.setActive(true);
+        document.setAdminEmailAddresses("jupp@ebi.ac.uk");
+        document.setActiveOntology("http://www.ebi.ac.uk/efo");
+        Set<String> ontologyImports= new HashSet<String>();
+        ontologyImports.add("http://www.ebi.ac.uk/efo/efo.owl");
+
+        document.setOntologyImports(ontologyImports);
+
+
+
+        PopulousDataRestriction dr1 = new PopulousDataRestriction(1, "Cell line");
+        dr1.setVariableName("?cellLine");
+        dr1.setRestrictionType(RestrictionType.DESCENDANTS);
+        dr1.setClassExpression("'cell line'");
+
+        PopulousDataRestriction dr2 = new PopulousDataRestriction(2, "cell type/line");
+        dr2.setVariableName("?cellType");
+        dr2.setRestrictionType(RestrictionType.DESCENDANTS);
+        dr2.setClassExpression("'cell type' or 'cell line'");
+
+        PopulousDataRestriction dr3 = new PopulousDataRestriction(3, "organism part");
+        dr3.setVariableName("?tissue");
+        dr3.setRestrictionType(RestrictionType.DESCENDANTS);
+        dr3.setClassExpression("'organism part'");
+
+        PopulousDataRestriction dr4 = new PopulousDataRestriction(4, "organism");
+        dr4.setVariableName("?species");
+        dr4.setClassExpression("organism");
+        dr4.setRestrictionType(RestrictionType.DESCENDANTS);
+
+        PopulousDataRestriction dr5 = new PopulousDataRestriction(5, "sex");
+        dr5.setVariableName("?sex");
+        dr5.setClassExpression("sex");
+        dr5.setRestrictionType(RestrictionType.DESCENDANTS);
+
+        PopulousDataRestriction dr6 = new PopulousDataRestriction(6, "disease");
+        dr6.setVariableName("?disease");
+        dr6.setClassExpression("disposition");
+        dr6.setRestrictionType(RestrictionType.DESCENDANTS);
+
+        PopulousDataRestriction dr7 = new PopulousDataRestriction(7, "definition");
+        dr7.setVariableName("?definition");
+        dr7.setRestrictionType(RestrictionType.UNRESTRICTED);
+
+        PopulousDataRestriction dr8 = new PopulousDataRestriction(8, "synonyms");
+        dr8.setVariableName("?synonym");
+        dr8.setMultivalueField(true);
+        dr8.setRestrictionType(RestrictionType.UNRESTRICTED);
+
+        PopulousDataRestriction dr9 = new PopulousDataRestriction(9, "definition citation");
+        dr9.setVariableName("?definitionCitation");
+        dr9.setRestrictionType(RestrictionType.UNRESTRICTED);
+
+        List<PopulousDataRestriction> restrictionSet = new ArrayList<PopulousDataRestriction>();
+        restrictionSet.add(dr1);
+        restrictionSet.add(dr2);
+        restrictionSet.add(dr4);
+        restrictionSet.add(dr5);
+        restrictionSet.add(dr6);
+        restrictionSet.add(dr7);
+        restrictionSet.add(dr8);
+        restrictionSet.add(dr9);
+        document.setDataRestrictions(restrictionSet);
+
+        PopulousPattern populousPattern1 = new PopulousPattern();
+        populousPattern1.setPatternName("Cell type, species, tissue");
+        populousPattern1.setPatternValue("?cellLine:CLASS, \n" +
+                "?cellType:CLASS, \n" +
+                "?species:CLASS, \n" +
+                "?tissue:CLASS \n" +
+                "BEGIN \n" +
+                "ADD ?cellLine subClassOf derives_from some (?cellType and part_of some (?tissue and part_of some ?species))\n" +
+                "END;");
+
+        PopulousPattern populousPattern2 = new PopulousPattern();
+        populousPattern2.setPatternName("Bearer of some disease");
+        populousPattern2.setPatternValue("?cellLine:CLASS, \n" +
+                "?disease:CLASS \n" +
+                "BEGIN \n" +
+                "ADD ?cellLine subClassOf bearer_of some ?disease \n" +
+                "END;");
+
+        PopulousPattern populousPattern3 = new PopulousPattern();
+        populousPattern3.setPatternName("Has quality some sex");
+        populousPattern3.setPatternValue("?cellLine:CLASS, \n" +
+                "?sex:CLASS \n" +
+                "BEGIN \n" +
+                "ADD ?cellLine subClassOf OBI_0000298 some ?sex \n" +
+                "END;\n");
+
+        PopulousPattern populousPattern4 = new PopulousPattern();
+        populousPattern4.setPatternName("Definition");
+        populousPattern4.setPatternValue("?cellLine:CLASS,\n" +
+                "?definition:CONSTANT\n" +
+                "BEGIN\n" +
+                "ADD ?cellLine.IRI definition \"?definition\"\n" +
+                "END;");
+
+        PopulousPattern populousPattern5 = new PopulousPattern();
+        populousPattern5.setPatternName("Synonyms");
+        populousPattern5.setPatternValue("?cellLine:CLASS,\n" +
+                "?synonym:CONSTANT\n" +
+                "BEGIN\n" +
+                "ADD ?cellLine.IRI alternative_term \"?synonym\"\n" +
+                "END;");
+
+        PopulousPattern populousPattern6 = new PopulousPattern();
+        populousPattern6.setPatternName("Definition citation");
+        populousPattern6.setPatternValue("?cellLine:CLASS,\n" +
+                "?definitionCitation:CONSTANT\n" +
+                "BEGIN\n" +
+                "ADD ?cellLine.IRI definition_citation \"?definitionCitation\"\n" +
+                "END;");
+
+        PopulousPattern populousPattern7 = new PopulousPattern();
+        populousPattern7.setPatternName("Cell line");
+        populousPattern7.setPatternValue("?cellLine:CLASS,\n" +
+                "BEGIN\n" +
+                "ADD ?cellLine subClassOf EFO_0000322\n" +
+                "END;");
+
+        List<PopulousPattern> patterns = new ArrayList<PopulousPattern>();
+        patterns.add(populousPattern1);
+        patterns.add(populousPattern2);
+        patterns.add(populousPattern3);
+        patterns.add(populousPattern4);
+        patterns.add(populousPattern5);
+        patterns.add(populousPattern6);
+        patterns.add(populousPattern7);
+        document.setPatterns(patterns);
+
+        System.out.println("template created about to save");
+
+        templateRepository.save(document);
+
+        System.out.println("saved!");
+    }
+
+    private void createPizza() {
         System.out.println("creating a new template");
         PopulousTemplateDocument document = new PopulousTemplateDocument();
         document.setDescription("Pizza template");
@@ -43,18 +198,12 @@ public class LoaderApp  implements CommandLineRunner {
         document.setAdminEmailAddresses("jupp@ebi.ac.uk");
         document.setActiveOntology("http://www.test.com/pizza.owl");
         Set<String> ontologyImports= new HashSet<String>();
-        URL ontologyURL = getClass().getClassLoader().getResource("pizza.owl");
-        ontologyImports.add("file:" + ontologyURL.getFile());
-//        ontologyImports.add("file:/Users/jupp/Dropbox/dev/ols/ontology-tools/src/test/resources/test-import-2.owl");
-
+        ontologyImports.add("http://www.ebi.ac.uk/~jupp/downloads/pizza.owl");
         document.setOntologyImports(ontologyImports);
-//        document.setLastUpdated(new Date());
-//        document.setStatus(Status.OK);
 
 
 
         PopulousDataRestriction dr1 = new PopulousDataRestriction(1, "Pizza");
-//        dr1.setClassExpression("A");
         dr1.setVariableName("?pizza");
         dr1.setRestrictionType(RestrictionType.UNRESTRICTED);
 
@@ -99,24 +248,13 @@ public class LoaderApp  implements CommandLineRunner {
         patterns.add(populousPattern2);
         document.setPatterns(patterns);
 
-        //        Map<String, Integer> variableToColumnMap = new HashMap<String, Integer>();
-        //        variableToColumnMap.put("?a", 1);
-        //        variableToColumnMap.put("?b", 2);
-        //        document.setVariableToColumnMap(variableToColumnMap);
-
         System.out.println("template created about to save");
 
-//        restrictionRunRepository.deleteAll();
-//        dataRunRepository.deleteAll();
-//        templateRepository.deleteAll();
-//        templateRepository.save(document);
+        templateRepository.save(document);
 
-        System.out.println("saved!");
-
-
-
-
+        System.out.println("saved pizza!");
     }
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(LoaderApp.class, args);
     }
